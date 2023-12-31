57d92f51917d8d69ff788d8c59067df9e626f46e
Rohandud <48047872+Rohandud@users.noreply.github.com>
2024-01-01 00:32:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
 from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
-# from testtele import send_message_to_user
+from kivy.clock import Clock
+import telebot
+
+bot = telebot.TeleBot('6549292499:AAHacZBWyJ8p2ZAmdZj94II941lF638UV7Q')  # Replace with your actual bot token
 
 class ButtonApp(App):
+    def send_message_to_user(self, txt):
+        USER_ID = 922758956
+        bot.send_message(USER_ID, txt)
+
     def build(self):
         layout = BoxLayout(orientation='vertical')
-        
+
         # Define two buttons
         button1 = Button(text='Button 1')
         button2 = Button(text='Button 2')
 
-        # Bind functions to the on_press and on_release events of the buttons
-        button1.bind(on_press=self.on_button_press, on_release=self.on_button_release)
-        button2.bind(on_press=self.on_button_press, on_release=self.on_button_release)
+        # Bind functions to the on_press events of the buttons
+        button1.bind(on_press=self.on_button_press)
+        button2.bind(on_press=self.on_button_press)
 
         # Add buttons to the layout
         layout.add_widget(button1)
@@ -24,11 +31,9 @@ class ButtonApp(App):
     def on_button_press(self, instance):
         # This function will be called when a button is pressed
         print(f'Button {instance.text} pressed')
-
-    def on_button_release(self, instance):
-        # This function will be called when a button is released
-        # send_message_to_user(f'Button {instance.text} pressed')
-        print(f'Button {instance.text} released')
+        
+        # Schedule sending a message to the user after a delay
+        Clock.schedule_once(lambda dt: self.send_message_to_user(f'Button {instance.text} pressed'), 1)
 
 if __name__ == '__main__':
     ButtonApp().run()
