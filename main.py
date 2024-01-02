92790301a78bf2ed0920913357c3d5ef0134f4c1
Rohandud <48047872+Rohandud@users.noreply.github.com>
2024-01-02 19:38:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,19 @@
-from kivy.app import App
+from kivymd.app import MDApp
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
-from kivy.clock import Clock
-import telebot
+from kivymd.toast import toast
 
-bot = telebot.TeleBot('6549292499:AAHacZBWyJ8p2ZAmdZj94II941lF638UV7Q')  # Replace with your actual bot token
-
-class ButtonApp(App):
-    def send_message_to_user(self, txt):
-        USER_ID = 922758956
-        bot.send_message(USER_ID, txt)
-
+class ButtonApp(MDApp):  # Change the inheritance to MDApp
     def build(self):
         layout = BoxLayout(orientation='vertical')
-
+        
         # Define two buttons
         button1 = Button(text='Button 1')
         button2 = Button(text='Button 2')
 
-        # Bind functions to the on_press events of the buttons
-        button1.bind(on_press=self.on_button_press)
-        button2.bind(on_press=self.on_button_press)
+        # Bind functions to the on_press and on_release events of the buttons
+        button1.bind(on_press=self.on_button_press, on_release=self.on_button_release)
+        button2.bind(on_press=self.on_button_press, on_release=self.on_button_release)
 
         # Add buttons to the layout
         layout.add_widget(button1)
@@ -31,9 +24,11 @@ class ButtonApp(App):
     def on_button_press(self, instance):
         # This function will be called when a button is pressed
         print(f'Button {instance.text} pressed')
-        
-        # Schedule sending a message to the user after a delay
-        Clock.schedule_once(lambda dt: self.send_message_to_user(f'Button {instance.text} pressed'), 1)
+
+    def on_button_release(self, instance):
+        # This function will be called when a button is released
+        print(f'Button {instance.text} released')
+        toast(f'Button {instance.text} released')
 
 if __name__ == '__main__':
     ButtonApp().run()
