fbd904907eaefc416dd5adeeb9a3823ec10f4256
Rohandud <48047872+Rohandud@users.noreply.github.com>
2024-01-02 20:59:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,50 +7,19 @@ from kivy.uix.textinput import TextInput
 
 class SayHello(App):
     def build(self):
-        #returns a window object with all it's widgets
         self.window = GridLayout()
         self.window.cols = 1
-        self.window.size_hint = (0.6, 0.7)
-        self.window.pos_hint = {"center_x": 0.5, "center_y":0.5}
-
-        # image widget
-        self.window.add_widget(Image(source="logo.png"))
-
-        # label widget
-        self.greeting = Label(
-                        text= "What's your name?",
-                        font_size= 18,
-                        color= '#00FFCE'
-                        )
+        self.window.add_widget(Image(source="wel.jpg"))
+        self.greeting = Label(text="Whats your name")
         self.window.add_widget(self.greeting)
-
-        # text input widget
-        self.user = TextInput(
-                    multiline= False,
-                    padding_y= (20,20),
-                    size_hint= (1, 0.5)
-                    )
-
+        self.user = TextInput(multiline=False)
         self.window.add_widget(self.user)
-
-        # button widget
-        self.button = Button(
-                      text= "GREET",
-                      size_hint= (1,0.5),
-                      bold= True,
-                      background_color ='#00FFCE',
-                      #remove darker overlay of background colour
-                      # background_normal = ""
-                      )
-        self.button.bind(on_press=self.callback)
+        self.button = Button(text="Greet")
         self.window.add_widget(self.button)
+
+        #add widgets to window
 
         return self.window
 
-    def callback(self, instance):
-        # change label text to "Hello + user name!"
-        self.greeting.text = "Hello " + self.user.text + "!"
-
-# run Say Hello App Calss
 if __name__ == "__main__":
     SayHello().run()
